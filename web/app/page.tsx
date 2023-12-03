6aab9fe5911f2e95871566423bb63b26bce78c5a
mkandil <mkandil@synapse-analytics.io>
2023-12-03 10:28:35 +0100

    add fist mvp

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,136 +1,49 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import Image from "next/image";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+"use client";
+import Pdf from "@/components/ui/Pdf";
+import { Card } from "@/components/ui/card";
+import Cards from "@/components/ui/cards";
+import { Chat } from "@/components/ui/cards";
+import useStore from "@/store/useStore";
+const url = "https://tum.support/sendmsg/lolttest";
+const username = "a";
+const password = "aZJnk_JwKYtqQa84dUFB";
+const encodedCredentials = Buffer.from(`${username}:${password}`).toString(
+  "base64"
+);
 
+async function getData() {
+  const res = await fetch(url, {
+    method: "GET",
+    credentials: "include",
+    headers: {
+      Authorization: `Basic ${encodedCredentials}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
+  return res;
+}
 export default function Home() {
+  const { toggle } = useStore();
   return (
-    <main className="flex min-h-screen bg-white flex-col items-center justify-center p-8">
-      <Card className="w-full rounded-[24px] bg-[#F1F5F9] border-0 px-8">
-        <div className="flex flex-col gap-2 py-8">
-          <h1 className="font-semibold text-4xl">Hello User</h1>
-          <p>Horem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className="flex flex-col min-h-screen dark:bg-gray-900">
-          <div className="flex flex-col w-full h-full justify-between">
-            <div className="flex flex-row justify-around w-full p-6 flex-wrap overflow-auto">
-              <Card className="flex flex-col w-72 h-auto rounded-xl p-6 bg-white shadow-lg dark:bg-gray-800 m-2">
-                <CardHeader>
-                  <CardTitle className="text-2xl font-bold text-gray-800 dark:text-white">
-                    Title of the Card
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col space-y-6 mt-6">
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 1
-                    </p>
-                  </Button>
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 2
-                    </p>
-                  </Button>
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 3
-                    </p>
-                  </Button>
-                </CardContent>
-              </Card>
-              <Card className="flex flex-col w-72 h-auto rounded-xl p-6 bg-white shadow-lg dark:bg-gray-800 m-2">
-                <CardHeader>
-                  <CardTitle className="text-2xl font-bold text-gray-800 dark:text-white">
-                    Title of the Card
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col space-y-6 mt-6">
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 1
-                    </p>
-                  </Button>
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 2
-                    </p>
-                  </Button>
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 3
-                    </p>
-                  </Button>
-                </CardContent>
-              </Card>
-              <Card className="flex flex-col w-72 h-auto rounded-xl p-6 bg-white shadow-lg dark:bg-gray-800 m-2">
-                <CardHeader>
-                  <CardTitle className="text-2xl font-bold text-gray-800 dark:text-white">
-                    Title of the Card
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col space-y-6 mt-6">
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 1
-                    </p>
-                  </Button>
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 2
-                    </p>
-                  </Button>
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 3
-                    </p>
-                  </Button>
-                </CardContent>
-              </Card>
-              <Card className="flex flex-col w-72 h-auto rounded-xl p-6 bg-white shadow-lg dark:bg-gray-800 m-2">
-                <CardHeader>
-                  <CardTitle className="text-2xl font-bold text-gray-800 dark:text-white">
-                    Title of the Card
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col space-y-6 mt-6">
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 1
-                    </p>
-                  </Button>
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 2
-                    </p>
-                  </Button>
-                  <Button className="bg-gray-100 p-4 rounded-full dark:bg-gray-700">
-                    <p className="text-sm text-gray-800 dark:text-gray-300">
-                      Content box 3
-                    </p>
-                  </Button>
-                </CardContent>
-              </Card>
-            </div>
-          </div>
-          <div className="flex justify-center py-4 dark:bg-gray-800">
-            <Input
-              aria-label="Search"
-              className="w-1/2 px-4 py-2 rounded-full text-gray-800 dark:text-gray-200 border border-gray-300 dark:border-gray-700 focus:outline-none focus:border-blue-500"
-              placeholder="Search..."
-              type="search"
-            />
+    <>
+      <main className="flex items-center justify-center p-8 bg-white h-screen w-screen">
+        <div className="w-screen relative h-[800px] rounded-[24px] bg-[#F1F5F9] overflow-auto p-6">
+          {toggle && (
+            <>
+              <h1 className="font-semibold text-4xl mb-4">Hello Leon!</h1>
+              <p className="mb-6">
+                Horem ipsum dolor sit amet, consectetur adipiscing elit.
+              </p>
+            </>
+          )}
+          <Cards />
+          <div className="flex justify-center items-end">
+            <Chat />
           </div>
         </div>
-      </Card>
-    </main>
+      </main>
+    </>
   );
 }
